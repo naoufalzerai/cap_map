c7651e65aa5ccea7c1ce2bde1049c39308a7f952
BEN BRAHIM <kamal.ben-brahim@capgemini.com>
2018-08-06 11:44:25 +0100

    KAMAL_06082018

--- cap_map/src/routes.js
+++ cap_map/src/routes.js
@@ -3,10 +3,12 @@ import { Route } from "react-router-dom";
 import { HomePage } from "./components/HomePage";
 import { MapBrowser } from "./components/MapBrowser";
 import { Layout } from "./components/Layout";
+import { SelectFloor } from "./components/SelectFloor"
 
 export const routes = (
   <Layout>
     <Route exact path="/" component={HomePage} />
     <Route path="/browse" component={MapBrowser} />
+    <Route path="/selectFloor" component={SelectFloor} />
   </Layout>
 );
