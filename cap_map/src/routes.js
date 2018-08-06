224813f771d8820884d91f6cfe1dec72acf7b87e
ZERAI <naoufal.zerai@capgemini.com>
2018-08-06 13:43:26 +0100

    merge nfl_kml

--- cap_map/src/routes.js
+++ cap_map/src/routes.js
@@ -5,7 +5,6 @@ import { MapBrowser } from "./components/MapBrowser";
 import { Layout } from "./components/Layout";
 import { SelectFloor } from "./components/SelectFloor"
 
-<<<<<<< HEAD
 export const routes = (
   <Layout>
     <Route exact path="/" component={HomePage} />
@@ -13,10 +12,3 @@ export const routes = (
     <Route path="/selectFloor" component={SelectFloor} />
   </Layout>
 );
-=======
-export const routes = 
-        <Layout>
-            <Route exact path="/" component={HomePage} />
-           <Route path="/browse" component={MapBrowser} />
-         </Layout>;
->>>>>>> c2be65af9c7e14333d18fd365139e8b114f972c0
