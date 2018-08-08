6b1d4aa2688363bc20b149f63f7bd42c04a44684
BEN BRAHIM <kamal.ben-brahim@capgemini.com>
2018-08-08 13:22:44 +0100

    Add Seats in maps

--- cap_map/src/components/MapBrowser.js
+++ cap_map/src/components/MapBrowser.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import * as d3 from "d3";
 import '../css/bootstrap.min.css';
 import maps from "../json/maps.json";
+import seats from "../json/seats.json";
 
 export class MapBrowser extends Component {
     constructor(props) {
         super(props);    
         this._click=this._click.bind(this)    
         this.state = { 
-            maps : maps
+            maps : maps,
+            seats : seats
           }    
     }
     
@@ -52,6 +54,7 @@ export class MapBrowser extends Component {
         });
     }
 
+
     SelSwitchMapChange(elm){
         var tab = this.state.maps.filter(function (map) {
           return map.uid == elm.target.value;
@@ -70,7 +73,88 @@ export class MapBrowser extends Component {
               document.getElementById("map-container").insertAdjacentHTML("afterbegin", res);
               //this.node.insertAdjacentHTML("afterbegin", res);
           });
-    
+
+          // add Seats
+          let selectedMap = tab[0].uid
+          let tabSeats = this.state.seats[selectedMap];
+          var map_container = d3.select("#map-container");
+        this.state.seats[tab[0].uid].map(function (seat) {
+            let node,text;
+            switch(seat.type){
+                case "H":
+                 node = map_container.append('g').classed('marker_wrapper', true).append("circle")
+                .classed(seat.uid, true)
+                .attr('uid', seat.uid)
+                .classed('fplan_marker', true)
+                .attr("cx", seat.coords[0])
+                .attr("cy", seat.coords[1])
+                .attr("fill", "red")
+                .attr("r", 10);
+                 text = d3.select(node.node().parentNode).append('text').attr('x', node.attr('cx')).attr('y', node.attr('cy')).attr('dy', '-.25em').text('').attr('fill', 'black');
+        
+                break;
+                case "WC":
+                     node = map_container.append('g').classed('marker_wrapper', true).append("circle")
+                     .classed(seat.uid, true)
+                     .attr('uid', seat.uid)
+                     .classed('fplan_marker', true)
+                     .attr("cx", seat.coords[0])
+                     .attr("cy", seat.coords[1])
+                     .attr("fill", "yellow")
+                     .attr("r", 10);
+                     text = d3.select(node.node().parentNode).append('text').attr('x', node.attr('cx')).attr('y', node.attr('cy')).attr('dy', '-.25em').text('').attr('fill', 'black');
+             
+                break;
+                case "Box":
+                    node = map_container.append('g').classed('marker_wrapper', true).append("rect")
+                    .classed(seat.uid, true)
+                    .attr('uid', seat.uid)
+                    .classed('fplan_marker', true)
+                    .attr("x", seat.coords[0])
+                    .attr("y", seat.coords[1])
+                    .attr("fill", "blue")
+                    .attr("stroke-width", 2)
+                    .attr("stroke", "rgb(0,0,0)")
+                    .attr("width", 20)
+                    .attr("height", 20);
+                    //width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)"
+                     text = d3.select(node.node().parentNode).append('text').attr('x', node.attr('cx')).attr('y', node.attr('cy')).attr('dy', '-.25em').text('').attr('fill', 'black');
+
+                break;
+                case "BoxL":
+                node = map_container.append('g').classed('marker_wrapper', true).append("rect")
+                    .classed(seat.uid, true)
+                    .attr('uid', seat.uid)
+                    .classed('fplan_marker', true)
+                    .attr("x", seat.coords[0])
+                    .attr("y", seat.coords[1])
+                    .attr("fill", "blue")
+                    .attr("stroke-width", 2)
+                    .attr("stroke", "rgb(0,0,0)")
+                    .attr("width", 40)
+                    .attr("height", 20);
+                    //width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)"
+                     text = d3.select(node.node().parentNode).append('text').attr('x', node.attr('cx')).attr('y', node.attr('cy')).attr('dy', '-.25em').text('').attr('fill', 'black');
+      
+                break;
+                case "BoxXL":
+                node = map_container.append('g').classed('marker_wrapper', true).append("rect")
+                    .classed(seat.uid, true)
+                    .attr('uid', seat.uid)
+                    .classed('fplan_marker', true)
+                    .attr("x", seat.coords[0])
+                    .attr("y", seat.coords[1])
+                    .attr("fill", "blue")
+                    .attr("stroke-width", 2)
+                    .attr("stroke", "rgb(0,0,0)")
+                    .attr("width", 60)
+                    .attr("height", 40);
+                     text = d3.select(node.node().parentNode).append('text').attr('x', node.attr('cx')).attr('y', node.attr('cy')).attr('dy', '-.25em').text('').attr('fill', 'black');
+      
+                break;
+            }
+        
+          })
 
       }
     
@@ -99,7 +183,7 @@ export class MapBrowser extends Component {
                   <path d="M0-1c-14.5-25.6-14.5-25.7-14.5-33.8c0-8.1,6.5-14.6,14.5-14.6s14.5,6.6,14.5,14.6C14.5-26.7,14.5-26.6,0-1z" />
                   <path d="M0-49c7.7,0,14,6.3,14,14.1c0,8,0,8.1-14,32.8c-14-24.7-14-24.9-14-32.8C-14-42.7-7.7-49,0-49 M0-50c-8.3,0-15,6.8-15,15.1 S-15-26.5,0,0c15-26.5,15-26.5,15-34.9S8.3-50,0-50L0-50z" />
                 </g>
-                <g id="map-container" />
+                <g id="map-container" className="#map-container" />
                 </defs>
                 <use href="#map-container" x={0} y={0} />
                 </svg>
